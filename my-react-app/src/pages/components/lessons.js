3b7195bb939435fccb7dc65e9b222e46341ab9a8
keen-ivan <i.mucyo@alustudent.com>
2025-03-29 15:26:30 +0200

    dashboard components folder and files

---
+++ my-react-app/src/pages/components/lessons.js
@@ -0,0 +1,9 @@
+function Lessons() {
+    return (
+        <section>
+            <h2>Lessons</h2>
+            <p>real-world cybersecurity lessons </p>
+        </section>
+    );
+}
+export default Lessons;
